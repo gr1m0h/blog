4bb2c0304b85814e3353370f9792b2b7283a8e8d
grimoh117 <misokyuuri.117@gmail.com>
2019-02-03 19:47:36 +0900

    add top page

---
+++ docs/.vuepress/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="post in posts">
+            <h2><a v-bind:href="post.path">{{post.title}}</a></h2>
+            <p>{{post.frontmatter.description}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+        posts() {
+            return this.$site.pages
+                // blogディレクトリ以下を投稿記事一覧表示の対象とする
+                .filter(post => post.path.startsWith('/blog/'))
+                // dateに設定した日付の降順にソートする
+                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+        }
+    }
+}
+</script>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	base: '/',
+	title: 'Grimoh',
+	description: ' ',
+	serviceWorker: true,
+	locales: {
+		'/': {
+			lang: 'ja'
+		}
+	},
+	meta: [
+		{ charset: 'utf-8' },
+		{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+	],
+	themeConfig: {
+		search: true,
+		docsDir: 'docs',
+		sidebar: 'auto',
+		lastUpdated: 'Last Updated',
+		serviceWorker: {
+      		updatePopup: {
+        		message   : "新しいコンテンツが追加されました。",
+        		buttonText: "更新",
+      		}
+    	},
+    	nav: [
+			{
+				text: 'About',
+				items: [
+					{text: 'Twitter', link: 'https://twitter.com/grimoh117'},
+      				{text: 'GitHub', link: 'https://github.com/grimoh117'}
+				]
+			}
+    	],
+		footer: '',
+  	}
+}
